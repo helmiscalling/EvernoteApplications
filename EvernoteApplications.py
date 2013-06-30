e9b0ae5f0c311484bf09716a08c5007791458407
helmiscalling <helmiscalling@gmail.com>
2013-06-30 17:23:03 -0500

    created findIndexes() in NotebookArchiveApplication

--- EvernoteApplications.py
+++ EvernoteApplications.py
@@ -23,13 +23,17 @@ class Library(object):
 		self.client = EvernoteClient(token = self.auth_token, sandbox = False)
 		self.noteStore = self.client.get_note_store()
 		self.userStore = self.client.get_user_store()
-		self.notebooks = self.noteStore.listNotebooks(self.auth_token)
-		self.tags = self.noteStore.listTags(self.auth_token)
+		self.notebooks = {}
+		for notebook in self.noteStore.listNotebooks(self.auth_token):
+			self.notebooks[notebook.name] = notebook.guid
+		self.tags = {}
+		for tag in self.noteStore.listTags(self.auth_token):
+			self.tags[tag.name] = tag.guid
 
 
 class Application(object):
 
-	def __init__(self, appName):
+	def __init__(self):
 		"""
 		Template class for generic application features.
 
@@ -37,19 +41,53 @@ class Application(object):
 			appName (string) Provides the application name
 		"""
 
-		self.name = appName
+		self.name = "Application"
 
 
 class NotebookArchiveApplication(Application):
 
-	def __init__(self, appName, library):
+	def __init__(self, library, indexTagGuid, pageTagGuid, notebookGuid):
 		"""
 		Initializes the application Notebook Archive
 
 		Arguments:
-			appName (string) Provides the application name
 			library (Library) Provides access to the Evernote library
+			notebookGuid (Guid) GUID of the notebook with the indexes and pages
+			indexTagGuid (Guid) GUID of the Index tag
+			pageTagGuid (Guid) GUID of the Page tag
 		"""
 
-		self.name = appName
+		self.name = "Notebook Archive"
 		self.library = library
+		self.notebookGuid = notebookGuid
+		self.indexTagGuid = indexTagGuid
+		self.pageTagGuid = pageTagGuid
+
+	def findIndexes(self, words):
+		"""
+		Find indexes that match a search, and return a dictionary
+		of the index titles and pages
+
+		Arguments:
+			words (String) Search terms based on Evernote's search grammar
+
+		Results:
+			(Dictionary) Dictionary of note titles with pages
+
+		searchFilter = NoteStore.NoteFilter()
+		searchFilter.words = words
+		searchFilter.notebookGuid = self.notebookGuid
+		searchFilter.tagGuids = [self.indexTagGuid]
+
+		searchSpec = NoteStore.NotesMetadataResultSpec()
+		searchSpec.includeTitle = True
+
+		foundNotes = self.library.noteStore.findNotesMetadata(self.library.auth_token, searchFilter, 0, 100, searchSpec)
+
+		results = {}
+		for note in foundNotes.notes:
+			firstPosition = 12
+			lastPosition = note.title.find(')')
+			results[note.title] = note.title[firstPosition:lastPosition]
+
+		return results
